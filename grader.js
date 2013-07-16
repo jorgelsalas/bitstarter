31f2a5899b3781ce73378951be1bd60f8cfc8dd4
John Smith <example@stanford.edu>
2013-07-16 21:13:29 +0000

    se agrega el estilo basico al sitio

--- grader.js
+++ grader.js
@@ -21,11 +21,15 @@ References:
    - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
 */
 
+var util = require('util');
 var fs = require('fs');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://evening-mesa-8058.herokuapp.com";
+var outfile = "out.txt";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -65,10 +69,28 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'Url to website', URL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    if (program.url != null){
+	 rest.get(program.url).on('complete', function(result) {
+                if (result instanceof Error) {
+                        sys.puts('Error: ' + result.message);
+                        this.retry(5000); // try again after 5 sec
+                } else {
+                        fs.writeFileSync(outfile, result);
+             		var checkJson = checkHtmlFile(outfile, program.checks);
+			var outJson = JSON.stringify(checkJson, null, 4);
+	                console.log(outJson);
+                }
+        });
+
+    } else {
+    
+    	var checkJson = checkHtmlFile(program.file, program.checks);
+    	var outJson = JSON.stringify(checkJson, null, 4);
+    	console.log(outJson);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
